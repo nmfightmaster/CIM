9a66c029e93b94d9576725ffe104c941523fcd74
Nathan Fightmaster <nmfightmaster@gmail.com>
2023-03-29 18:00:15 -0700

    adding Data/Configurations folder and implementing DeviceConfiguration class

---
+++ CIM/CIM/Data/Configurations/DeviceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CIM.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CIM.Data.Configurations
+{
+    public class DeviceConfiguration : IEntityTypeConfiguration<Device>
+    {
+        public void Configure(EntityTypeBuilder<Device> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(9);
+            builder.Property(x => x.ServiceTag)
+                .HasMaxLength(7);
+                
+        }
+    }
+    {
+    }
+}
