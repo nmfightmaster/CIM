46c3c3f4c63cc38efe3914759cd1da7ddc83298b
Nathan Fightmaster <nmfightmaster@gmail.com>
2023-03-29 17:14:51 -0700

    adding Data folder and creating CIMContext class

---
+++ CIM/CIM/Data/CIMContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CIM.Data
+{
+    public class CIMContext : DbContext
+    {
+        public CIMContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+    }
+}
