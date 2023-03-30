027ba210d6e4a48fe94ded1f317742601d6a5e68
Nathan Fightmaster <nmfightmaster@gmail.com>
2023-03-29 17:05:59 -0700

    adding Models folder and creating empty Device class

---
+++ CIM/CIM/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.ObjectPool;
+
+namespace CIM.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ServiceTag { get; set; }
+        public string? OU { get; set; }
+        public string? PU { get; set; }
+
+        public string[]? Issues { get; set; }
+    }
+}
