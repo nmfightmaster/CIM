4847de2e55c0d19ada04ff14d79b397047f5c9d3
Nathan Fightmaster <nmfightmaster@gmail.com>
2023-03-29 17:22:06 -0700

    registering context with service container

--- CIM/CIM/Program.cs
+++ CIM/CIM/Program.cs
@@ -1,8 +1,14 @@
+using CIM.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<CIMContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString
+        ("CIMContext"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
