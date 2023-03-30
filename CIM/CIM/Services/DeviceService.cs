e51e2e6f9f48b65002fb4760819e8319ffb4158d
Nathan Fightmaster <nmfightmaster@gmail.com>
2023-03-30 12:11:03 -0700

    implementing DeviceService

--- CIM/CIM/Services/DeviceService.cs
+++ CIM/CIM/Services/DeviceService.cs
@@ -1,6 +1,22 @@
-﻿namespace CIM.Services
+﻿using CIM.Data;
+using CIM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIM.Services
 {
-    public class DeviceService
+    public class DeviceService : IDeviceService
     {
+        private readonly CIMContext _context;
+        public DeviceService(CIMContext context) => _context = context;
+        public async Task<List<Device>> GetAllAsync()
+        {
+            var devices = _context.Devices
+                .Include(c => c.Name)
+                .Include(c => c.ServiceTag)
+                .Include(c => c.OU)
+                .Include(c => c.PU)
+                .Include(c => c.PI);
+            return await devices.ToListAsync();
+        }
     }
 }
