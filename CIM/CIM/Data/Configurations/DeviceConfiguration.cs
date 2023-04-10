267223716ff60ef965e66b095c2787d000d6ee9b
Nathan Fightmaster <nfightmaster@gmail.com>
2023-04-10 11:46:12 -0700

    removing 'previous issues' device property and moving towards database of issues

--- CIM/CIM/Data/Configurations/DeviceConfiguration.cs
+++ CIM/CIM/Data/Configurations/DeviceConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using CIM.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections;
 
 namespace CIM.Data.Configurations
 {
@@ -14,9 +15,9 @@ namespace CIM.Data.Configurations
                 .HasMaxLength(7);
             builder.HasData(new List<Device>
             {
-                new Device { Id = 1,Name = "CHASTEST",ServiceTag = "1234abc",OU="\\chas.local\\CLINIC\\DEPT\\",PU="Biggles T. McClure",PI="monitor issues",Status="Deployable"},
-                new Device { Id = 2,Name = "CHASTEST2",ServiceTag = "5678def",OU="\\chas.local\\CLINIC2\\DEPT2\\",PU="Bo 'Bobo' BoBoBo",PI="wifi issues",Status="Needs Imaged"},
-                new Device { Id = 3,Name = "CHASTEST3",ServiceTag = "910ghij",OU="\\chas.local\\CLINIC3\\DEPT3\\",PU="Hugh Mungus",PI="self esteem issues",Status="Deployed"}
+                new Device { Id = 1,Name = "CHASTEST",ServiceTag = "1234abc",OU="\\chas.local\\CLINIC\\DEPT\\",PU="Biggles T. McClure",Status="Deployable"},
+                new Device { Id = 2,Name = "CHASTEST2",ServiceTag = "5678def",OU="\\chas.local\\CLINIC2\\DEPT2\\",PU="Bo 'Bobo' BoBoBo",Status="Needs Imaged"},
+                new Device { Id = 3,Name = "CHASTEST3",ServiceTag = "910ghij",OU="\\chas.local\\CLINIC3\\DEPT3\\",PU="Hugh Mungus",Status="Deployed"}
             });
                 
         }
--- CIM/CIM/Models/Device.cs
+++ CIM/CIM/Models/Device.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.ObjectPool;
 using System.ComponentModel;
 using System.Text.Json.Serialization;
+using System.Collections;
 
 namespace CIM.Models
 {
@@ -15,8 +16,6 @@ namespace CIM.Models
         public string? OU { get; set; }
         [DisplayName("Primary User")]
         public string? PU { get; set; }
-        [DisplayName("Previous Issue")]
-        public string? PI { get; set; }
         public string Status { get; set; }
 
     }
