6581dfba68a9ab538927cf751b3918ad9c22d02d
Nathan Fightmaster <nfightmaster@gmail.com>
2023-04-10 15:13:00 -0700

    adding previousissue class

---
+++ CIM/CIM/Models/PreviousIssue.cs
@@ -0,0 +1,11 @@
+﻿namespace CIM.Models
+{
+    public class PreviousIssue
+    {
+        public int Id { get; set; }
+        public string IssueType { get; set; }
+        public string IssueDetails { get; set; }
+        public string Technician { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+}
