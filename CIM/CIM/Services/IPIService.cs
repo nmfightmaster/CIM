b971c27aaab05aca2b4e27732096e7a326dac526
Nathan Fightmaster <nfightmaster@gmail.com>
2023-04-11 16:20:17 -0700

    implementing PIService and corresponding interface

---
+++ CIM/CIM/Services/IPIService.cs
@@ -0,0 +1,12 @@
+﻿using CIM.Models;
+
+namespace CIM.Services
+{
+    public interface IPIService
+    {
+        Task<PreviousIssue> CreateAsync(PreviousIssue previousIssue);
+        Task<List<PreviousIssue>> GetAllAsync();
+        Task<PreviousIssue> FindAsync(int id);
+        Task<PreviousIssue> UpdateAsync(PreviousIssue previousIssue);
+    }
+}
---
+++ CIM/CIM/Services/PIService.cs
@@ -0,0 +1,40 @@
+﻿using CIM.Data;
+using CIM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIM.Services
+{
+    public class PIService : IPIService
+    {
+        private readonly CIMContext _context;
+
+        public PIService(CIMContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PreviousIssue> FindAsync(int id)
+        {
+            return await _context.PreviousIssues
+                .FindAsync(id);
+        }
+        public async Task<List<PreviousIssue>> GetAllAsync()
+        {
+            return await _context.PreviousIssues
+                .Include(pi => pi.Device)
+                .ToListAsync();
+        }
+        public async Task<PreviousIssue> CreateAsync(PreviousIssue previousIssue)
+        {
+            _context.Add(previousIssue);
+            await _context.SaveChangesAsync();
+            return previousIssue;
+        }
+        public async Task<PreviousIssue> UpdateAsync(PreviousIssue previousIssue)
+        {
+            _context.Update(previousIssue);
+            await _context.SaveChangesAsync();
+            return previousIssue;
+        }
+    }
+}
