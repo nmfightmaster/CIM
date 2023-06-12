083274f0187593627ef12647718b2a9049811ffd
Nathan Fightmaster <nfightmaster@gmail.com>
2023-06-12 14:47:38 -0700

    adding styling

--- CIM/reactfrontend/src/components/Computer.jsx
+++ CIM/reactfrontend/src/components/Computer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Computer = (props) => {
   const [computer, setComputer] = useState(null);
+  const [expanded, setExpanded] = useState(false);
   useEffect(() => {
     fetch(`http://localhost:3001/api/computers/${props.name}`)
       .then((response) => response.json())
@@ -9,9 +10,30 @@ const Computer = (props) => {
         setComputer(data);
       });
   }, []);
-
   return (
     <>
+      <div
+        onClick={() => setExpanded(!expanded)}
+        className="flex flex-wrap border rounded-3xl py-2 px-2 hover:bg-gray-600"
+      >
+        {!expanded && (
+          <>
+            <div className="w-1/3">{computer.name}</div>
+            <div className="w-1/3">{computer.serviceTag}</div>
+            <div className="w-1/3">{computer.model}</div>
+          </>
+        )}
+        {expanded && (
+          <div className="w-full flex">
+            <div className="w-1/3">Status: {computer.status}</div>
+            <div className="w-1/3">Last Imaged: {computer.imagedOn}</div>
+            <div className="w-1/3">
+              Warranty Expiration: {computer.warranty}
+            </div>
+          </div>
+        )}
+      </div>
+      {/* ------------------------------------------------ */}
       <br />
       <br />
       <br />
